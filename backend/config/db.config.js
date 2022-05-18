60441eb9e9d671ea6bf988d601be509eb9306a48
Piilu <piilrainer@gmail.com>
2022-05-18 20:58:08 +0300

    added .env

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 module.exports = {
-    HOST: "192.168.1.139",
-    USER: "data",
-    PASSWORD: "teretere",
-    DB: "arp",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: "mysql",
     pool: {
       max: 5,
